468ab8627923dbf3c183a338eb06612de64c81b3
Praveen Vikram <praveen.vikram@kiosa.org>
2019-09-27 10:07:26 -0400

    update erlang tools path

--- modules/programming.el
+++ modules/programming.el
@@ -174,7 +174,7 @@
 
 ;; erlang mode
 
-(setq load-path (cons "/usr/lib/erlang/lib/tools-3.2/emacs" load-path))
+(setq load-path (cons "/usr/lib/erlang/lib/tools-3.2.1/emacs" load-path))
 (require 'erlang-start)
 (setq erlang-root-dir "/usr/lib/erlang/")
 (setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
