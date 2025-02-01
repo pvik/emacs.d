4b6e66e02d70081f659f188e7f877ced8b47074e
Praveen Vikram <praveen.vikram@kiosa.org>
2025-01-31 22:32:42 -0500

    theme for gui and cli

--- init.el
+++ init.el
@@ -264,7 +264,9 @@
   :config
   (setq doom-themes-enable-bold nil  ; if nil, bold is universally disabled
 		doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+  (if (display-graphic-p)
+	  (load-theme 'doom-one t) ;; GUI Theme
+	  (load-theme 'doom-dracula t))
   (doom-themes-visual-bell-config) ;; Enable flashing mode-line on errors
   (doom-themes-neotree-config) ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-org-config) ;; Corrects (and improves) org-mode's native fontification.
