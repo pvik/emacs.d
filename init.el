62541038106a7cab843cd9d7184a51f6a75ed44b
Praveen Vikram <praveen.vikram@kiosa.org>
2025-09-25 16:44:30 -0400

    ckpt

--- init.el
+++ init.el
@@ -631,3 +631,6 @@
 (setq gc-cons-threshold gc-cons-threshold-orig)
 
 ;;; init.el ends here
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
