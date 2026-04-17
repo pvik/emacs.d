74d376ac43979e0162a4906757cfaa1a3153a22b
Praveen Vikram <praveen.vikram@kiosa.org>
2026-04-17 14:44:48 -0400

    update gptel config

--- modules/llm.el
+++ modules/llm.el
@@ -8,24 +8,23 @@
 (use-package gptel
   :ensure t
   :config
-  (setq gptel-api-key pvik-openai-token)
+  ;; (setq gptel-api-key pvik-openai-token)
   ;; OpenRouter offers an OpenAI compatible API
-  (gptel-make-openai "OpenRouter"               ;Any name you want
-	:host "openrouter.ai"
-	:endpoint "/api/v1/chat/completions"
-	:stream t
-	:key pvik-openrouter-token                  ;can be a function that returns the key
-	:models '(openai/gpt-3.5-turbo
-			  anthropic/claude-sonnet-4.6
-			  anthropic/claude-haiku-4.5
-			  moonshotai/kimi-k2.5
-			  qwen/qwen3.5-9b
-			  qwen/qwen3.5-122b-a10b
-              mistralai/mixtral-8x7b-instruct
-              meta-llama/codellama-34b-instruct
-              codellama/codellama-70b-instruct
-              google/palm-2-codechat-bison-32k
-              google/gemini-pro))
+  (setq gptel-model   'gemma-4-31b-it
+		gptel-backend
+		(gptel-make-openai "OpenRouter"               ;Any name you want
+		  :host "openrouter.ai"
+		  :endpoint "/api/v1/chat/completions"
+		  :stream t
+		  :key pvik-openrouter-token                  ;can be a function that returns the key
+		  :models '(anthropic/claude-opus-4.7
+					anthropic/claude-sonnet-4.6
+					anthropic/claude-haiku-4.5
+					openai/gpt-5.4
+					z-ai/glm-5.1
+					google/gemma-4-31b-it
+					google/gemini-3.1-pro-preview
+					moonshotai/kimi-k2.5)))
   ;; (gptel-make-anthropic "Claude"
   ;; 						:stream t
   ;; 						:key pvik-anthropic-token)
